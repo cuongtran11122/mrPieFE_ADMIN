63a2310e8ddc5beeb73a28c1432eee3bb50cd552
Khoa Pham <phamvudangkhoa2002@gmail.com>
2024-04-25 11:47:48 +0700

    Re-fix Category screen

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return ( 
         <footer className="main-footer">
-            <strong>Copyright © 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
+            <strong>Copyright © 2023-2024 <span className='text-primary'>Mr.PIE Admin</span>. </strong>
             All rights reserved.
             <div className="float-right d-none d-sm-inline-block">
                 <b>Version</b> 3.0.5
