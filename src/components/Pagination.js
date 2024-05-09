ea801fd6f1428824159527f890c1412def938f03
Thang Do Quang <20521893@gm.uit.edu.vn>
2024-05-09 15:41:57 +0700

    update pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,6 +1,22 @@
 import React from "react";
 
 const Pagination = ({ pages, page, setPage }) => {
+    let startPage = 1;
+    let endPage = Math.min(pages, 5); // Hiển thị tối đa 5 phân trang
+
+    if (page > 3) {
+        startPage = page - 2;
+        endPage = Math.min(page + 2, pages);
+    }
+
+    if (endPage - startPage < 4 && pages > 5) {
+        startPage = endPage - 4;
+    }
+
+    const handlePageClick = (pageNumber) => {
+        setPage(pageNumber);
+    };
+
     return (
         pages > 1 && (
             <nav>
@@ -8,32 +24,51 @@ const Pagination = ({ pages, page, setPage }) => {
                     <li className="page-item">
                         <button
                             className="page-link"
-                            onClick={() => setPage(page - 1)}
+                            onClick={() => handlePageClick(page - 1)}
                             disabled={page < 2}
                         >
                             Previous
                         </button>
                     </li>
-                    {[...Array(pages).keys()].map((x) => (
+                    {startPage > 1 && (
+                        <li className="page-item">
+                            <button
+                                className={`page-link `}
+                                onClick={() => handlePageClick(startPage - 1)}
+                            >
+                                ...
+                            </button>
+                        </li>
+                    )}
+                    {[...Array(endPage - startPage + 1).keys()].map((index) => (
                         <li
-                            key={`page${x}`}
+                            key={`page${startPage + index}`}
                             className={`page-item ${
-                                x + 1 === page ? "active" : ""
+                                startPage + index === page ? "active" : ""
                             }`}
                         >
                             <button
                                 className={`page-link `}
-                                onClick={() => setPage(x + 1)}
+                                onClick={() => handlePageClick(startPage + index)}
                             >
-                                {x + 1}
+                                {startPage + index}
                             </button>
                         </li>
                     ))}
-
+                    {endPage < pages && (
+                        <li className="page-item">
+                            <button
+                                className={`page-link `}
+                                onClick={() => handlePageClick(endPage + 1)}
+                            >
+                                ...
+                            </button>
+                        </li>
+                    )}
                     <li className="page-item">
                         <button
                             className="page-link"
-                            onClick={() => setPage(page + 1)}
+                            onClick={() => handlePageClick(page + 1)}
                             disabled={page >= pages}
                         >
                             Next
